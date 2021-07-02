cd41fe3d0443eccd288652b731e3a4484e8b9208
sidgupta11 <86286322+sidgupta11@users.noreply.github.com>
2021-07-02 13:51:24 +0530

    test commit

--- vogon-web/src/test/java/org/zlogic/vogon/web/configuration/VogonConfigurationDiffblueTest.java
+++ vogon-web/src/test/java/org/zlogic/vogon/web/configuration/VogonConfigurationDiffblueTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @ContextConfiguration(classes = {VogonConfiguration.class})
 @RunWith(SpringRunner.class)
 public class VogonConfigurationDiffblueTest {
